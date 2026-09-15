82dcab4a8366dbba9fc537a79440a60228049e3d
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-09-16 00:45:18 +0500

    Add files via upload

---
+++ ahsan/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.Intrinsics.X86;
+
+public class A {
+    public static void Main(string[] args) {
+
+        Console.WriteLine("========== ATM =========");
+        double pin = 1234;
+        float balance = 22000;
+        Console.WriteLine("Enter the pin...");
+        double upin = Convert.ToInt32(Console.ReadLine());
+        if (upin == pin)
+        {
+            Console.WriteLine("Login Successfull");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Balance  :  " + balance);
+            Console.WriteLine("----------------------");
+        
+
+}
+        else
+        {
+            Console.WriteLine("Invalid pin");
+        }
+
+    }
+}
+
+
+
