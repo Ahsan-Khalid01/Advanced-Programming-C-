be6aa3ad80bbebc4dc155b2fe7150aac19725ffb
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-09-16 00:46:21 +0500

    Add files via upload

---
+++ TestLab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.Intrinsics.X86;
+
+Console.WriteLine("========== ATM =========");
+int pin = 1234;
+float balance = 22000;
+Console.WriteLine("Enter the pin...");
+int upin = Convert.ToInt32(Console.ReadLine);
+if (upin == pin)
+{
+    Console.WriteLine("Login Successfull");
+    Console.WriteLine("---------------------------");
+    Console.WriteLine("Balance  :  " + balance);
+    Console.WriteLine("----------------------")
+
+
+}
+else
+{
+    Console.WriteLine("Invalid pin");
+}
+
+
+
